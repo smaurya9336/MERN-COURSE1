0431e12a3335e626e7d23b513375dc131b9ece08
Sarita <sm7767119@gmail.com>
2024-03-09 19:28:43 +0530

    String and operator

---
+++ JAVASCRIPT/Index.js
@@ -0,0 +1,28 @@
+let isLoggedIn = "Sarita";
+
+let booleanIsLoogedIn = Boolean(isLoggedIn);
+//console.log(booleanIsLoogedIn);
+// 1= true; 0=false
+//""=false ; "sarita"=true;
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// *********************** Operations ************************
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+let str1 = "hello";
+let str2 = " Sarita";
+let str3 = str1 + str2;
+//console.log(str3);
+
+// console.log(+true); //1
+
+// let num1 = 4;
+// console.log(num1);
