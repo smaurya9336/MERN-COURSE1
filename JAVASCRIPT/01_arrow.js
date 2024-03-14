ff067423fbed81b73dc6a33711d26b8ee142a8b6
Sarita <sm7767119@gmail.com>
2024-03-14 22:57:16 +0530

    IIFE

--- JAVASCRIPT/01_arrow.js
+++ JAVASCRIPT/01_arrow.js
@@ -14,7 +14,43 @@ const user = {
 
 // console.log(this);
 
-function Username() {
+// function Username() {
+//   let userName = "Sarita";
+//   console.log(this.UserName);
+// }
+// Username();
+
+// const chai = function () {
+//   let username = "hitesh";
+//   console.log(this.username);
+// };
+
+const chai = () => {
+  let username = "hitesh";
+  //   console.log(this.username);
   console.log(this);
-}
-username();
+};
+
+// chai();
+
+// const addTwo = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// const addTwo = (num1, num2) => num1 + num2;
+
+// const addTwo = (num1, num2) => num1 + num2;
+
+const addTwo = (num1, num2) => ({ user: "sarita" });
+
+// console.log(addTwo(7, 9));
+
+//IIFE
+(function chai() {
+  // named IIFE
+  console.log(`DB connected`);
+})();
+
+((name) => {
+  console.log(`DB CONNECTED TWO ${name}`);
+})("SARITA");
