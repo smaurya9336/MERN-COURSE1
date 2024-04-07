c455d8a25da2eb35d562e2c95530a64ce6337ef5
Sarita <sm7767119@gmail.com>
2024-04-07 14:06:49 +0530

    inheritance

---
+++ JAVASCRIPT/OOP_Concept/inheritance.js
@@ -0,0 +1,22 @@
+class User {
+  constructor(username) {
+    this.username = username;
+    // console.log("called");
+  }
+  logMe() {
+    console.log(`Username is ${this.username}`);
+  }
+}
+class Teacher extends User {
+  constructor(username, email, password) {
+    super(username);
+    this.email = email;
+    this.password = password;
+  }
+  addCourse() {
+    console.log(`A new course was added by ${this.username}`);
+  }
+}
+
+const demo = new Teacher("Sarita Maurya", "sm@teacher.com", "123");
+demo.addCourse();
