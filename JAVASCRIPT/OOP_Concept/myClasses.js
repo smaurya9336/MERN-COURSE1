0d277b9cc204b60b1852d8fafc95fd8769492ae2
Sarita <sm7767119@gmail.com>
2024-04-07 13:46:26 +0530

    behind the scene

--- JAVASCRIPT/OOP_Concept/myClasses.js
+++ JAVASCRIPT/OOP_Concept/myClasses.js
@@ -1,19 +1,36 @@
 // ES6
-class User {
-  constructor(username, email, password) {
-    this.username = username;
-    this.email = email;
-    this.password = password;
-  }
-  encryptPaasword() {
-    return `${this.password}abc`;
-  }
-  changeUsername() {
-    return `${this.username.toUpperCase()}`;
-  }
+// class User {
+//   constructor(username, email, password) {
+//     this.username = username;
+//     this.email = email;
+//     this.password = password;
+//   }
+//   encryptPaasword() {
+//     return `${this.password}abc`;
+//   }
+//   changeUsername() {
+//     return `${this.username.toUpperCase()}`;
+//   }
+// }
+// const demo = new User("sarita", "sarita@gmail.com", "123");
+
+// console.log(demo.encryptPaasword());
+// console.log(demo.changeUsername());
+
+// behind the scene
+
+function User(username, email, password) {
+  this.username = username;
+  this.email = email;
+  this.password = password;
 }
+User.prototype.encryptPaasword = function () {
+  return `${this.password}abc`;
+};
+User.prototype.changeUsername = function () {
+  return `${this.username.toUpperCase()}`;
+};
 
-const demo = new User("sarita", "sarita@gmail.com", "123");
-
-console.log(demo.encryptPaasword());
-console.log(demo.changeUsername());
+const demo2 = new User("harsh", "harsh@lnt.com", "1234");
+console.log(demo2.encryptPaasword());
+console.log(demo2.changeUsername());
