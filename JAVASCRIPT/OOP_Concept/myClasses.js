6c05b64f5adbca15d2aa0faeb35baf69eae22d34
Sarita <sm7767119@gmail.com>
2024-04-07 13:39:38 +0530

    class constructor

--- JAVASCRIPT/OOP_Concept/myClasses.js
+++ JAVASCRIPT/OOP_Concept/myClasses.js
@@ -1,1 +1,19 @@
 // ES6
+class User {
+  constructor(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+  encryptPaasword() {
+    return `${this.password}abc`;
+  }
+  changeUsername() {
+    return `${this.username.toUpperCase()}`;
+  }
+}
+
+const demo = new User("sarita", "sarita@gmail.com", "123");
+
+console.log(demo.encryptPaasword());
+console.log(demo.changeUsername());
